aefb6fbfab7f41af643280657ae226ea0ee323a6
Austin Clift <85080666+ausclift@users.noreply.github.com>
2025-10-30 00:06:08 -0400

    errors

--- app/Labs/Lab4/page.tsx
+++ app/Labs/Lab4/page.tsx
@@ -1,43 +1,11 @@
-"use client";
-import PassingFunctions from "./PassingFunctions";
-import ClickEvent from "./ClickEvent";
-import PassingDataOnEvent from "./PassingDataOnEvent";
-import EventObject from "./EventObject";
-import Counter from "./Counter";
-import BooleanStateVariables from "./BooleanStateVariables";
-import StringStateVariables from "./StringStateVariables";
-import DateStateVariable from "./DateStateVariables";
-import ObjectStateVariable from "./ObjectStateVariables";
-import ArrayStateVariable from "./ArrayStateVariables";
-import ParentStateComponent from "./ParentStateComponent";
-import ReduxExamples from "./ReduxExamples/page";
-import store from "./store";
-import { Provider } from "react-redux";
-
 export default function Lab4() {
-  function sayHello() {
-    alert("Hello");
-  }
-
   return (
-    <div id="wd-lab1">
-      <Provider store={store}>
-        <h2>Lab 4</h2>
-        <h3>Austin Clift - Section 05</h3>
+    <div id="wd-lab4">
+      <h2>Lab 4</h2>
+      <h3>Austin Clift - Section 05</h3>
 
-        <ClickEvent />
-        <PassingDataOnEvent />
-        <PassingFunctions theFunction={sayHello} />
-        <EventObject />
-        <Counter />
-        <BooleanStateVariables />
-        <StringStateVariables />
-        <DateStateVariable />
-        <ObjectStateVariable />
-        <ArrayStateVariable />
-        <ParentStateComponent />
-        <ReduxExamples />
-      </Provider>
+
+
     </div>
     );
 }
